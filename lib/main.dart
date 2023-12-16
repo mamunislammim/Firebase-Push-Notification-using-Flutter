38db7ef4c68ddd90d587c3167fc163f3593142d9
mamunislammim <amarsopno678@gmail.com>
2023-12-16 22:59:25 +0600

    Firebase add

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_push_notification_firebase/views/pages/home/home_page.dart';
+
+import 'controllers/Firebase/firebase_options.dart';
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+          appBarTheme: const AppBarTheme(
+              backgroundColor: Colors.teal,
+              titleTextStyle: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
+              centerTitle: true),
+      ),
+      home: const HomePage(),
+    );
+  }
+}
